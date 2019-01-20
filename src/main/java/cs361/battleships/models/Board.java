263a16ee93ee4bf0d3ea2d95b28e6b63cdb4867e
Sowanicj <sowanicj@oregonstate.edu>
2019-01-19 18:09:04 -0800

    Added validation for board.placeShip
    Place ship now tests for if the ship is over the edge of the board, and based on the XY coordinate and the length of the ship

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -17,7 +17,33 @@ public class Board {
 	 */
 	public boolean placeShip(Ship ship, int x, char y, boolean isVertical) {
 		// TODO Implement
-		return false;
+		int len;
+		switch (ship.getShipName()) {
+			case "MINESWEEPER":
+				len = 2;
+				break;
+			case "DESTROYER":
+				len = 3;
+				break;
+			case "BATTLESHIP":
+				len = 4;
+				break;
+			default:
+				return false;
+		}
+		for (int i = 0; i < len; i++) {
+			if (isVertical) {
+				if (x + i > 10 || x + i <= 0 || y > 'J' || y <= 0) {
+					return false;
+				}
+			}
+			else {
+				if (x > 10 || x <= 0 || y + i > 'J' || y + i <= 0) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 	/*
--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -8,17 +8,24 @@ import java.util.List;
 public class Ship {
 
 	@JsonProperty private List<Square> occupiedSquares;
+	@JsonProperty private String shipName;
 
 	public Ship() {
 		occupiedSquares = new ArrayList<>();
 	}
 	
 	public Ship(String kind) {
+		occupiedSquares = new ArrayList<>();
+		shipName = kind;
 		//TODO implement
 	}
 
 	public List<Square> getOccupiedSquares() {
 		//TODO implement
 		return null;
+	}
+
+	public String getShipName() {
+		return shipName;
 	}
 }
