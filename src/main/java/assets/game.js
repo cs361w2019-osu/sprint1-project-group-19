d1aac5d443d5f905be3c8f5d806751ef7a8bd587
pingnomw <pingnomw@oregonstate.edu>
2019-02-07 21:26:40 -0800

    Added a description about the opponent's attack time in the status box

--- src/main/java/assets/game.js
+++ src/main/java/assets/game.js
@@ -158,5 +158,5 @@ function attackPhase(){
     document.getElementById("is_vertical").style.display = "none";
     document.getElementById("vertical_caption").style.display = "none";
     document.getElementById("vertical_caption").style.display = "none";
-    document.getElementById("status").innerHTML = "<h3>Attack phase</h3>Click on a square on the opponent's board to attack it."
+    document.getElementById("status").innerHTML = "<h3>Attack phase</h3>Click on a square on the opponent's board to attack it.<br>The opponent will attack your board as you attack theirs."
 }
